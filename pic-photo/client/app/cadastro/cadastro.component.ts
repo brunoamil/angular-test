7b37ea124f1c6c39efb6ba9f761f4b3f71171349
Bruno Lima <bruno-amil@hotmail.com>
2020-06-10 19:47:52 -0300

    trabalhando com jquery e criacao de modal

--- pic-photo/client/app/cadastro/cadastro.component.ts
+++ pic-photo/client/app/cadastro/cadastro.component.ts
@@ -1,20 +1,39 @@
 import { Component} from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
-import { Http, Headers } from '@angular/http';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FotoService } from '../foto/foto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
     templateUrl: './cadastro.component.html'
 })
+
 export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
-    http: Http;
     meuForm: FormGroup;
+    service: FotoService;
+    route: ActivatedRoute;
+    router: Router;
+    mensagem: string = '';
 
-    constructor(http: Http, fb: FormBuilder){
-        this.http = http;
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute, router: Router){
+        
+        this.service = service;
+        this.route = route;
+        this.router = router;
+
+        this.route.params.subscribe(params => {
+            let id = params['id'];
+
+            if(id) {
+                this.service
+                .buscaPorId(id)
+                .subscribe(foto => this.foto = foto, erro => console.log(erro));
+                }
+        });
+
         this.meuForm =fb.group({
             titulo: ['',Validators.compose([Validators.required, Validators.minLength(4)])],
             url: ['',Validators.required],
@@ -24,14 +43,15 @@ export class CadastroComponent {
     cadastrar(event){
         
         event.preventDefault();
-        console.log(this.foto);
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-
-        this.http.post('v1/fotos', JSON.stringify(this.foto), { headers: headers })
-        .subscribe(() => {
-            this.foto = new FotoComponent();
-            console.log("Foto salva com sucesso");
-        }, erro => console.log(erro));
+        //console.log(this.foto);
+        
+        this.service
+            .cadastra(this.foto)
+            .subscribe(res => {
+                this.mensagem = res.mensagem;
+                this.foto = new FotoComponent();
+                if(!res.inclusao) this.router.navigate(['']);
+            }, erro => console.log(erro));
+        
     }
 }
--- pic-photo/client/app/listagem/listagem.component.ts
+++ pic-photo/client/app/listagem/listagem.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { FotoService } from '../foto/foto.service';
+import { FotoComponent } from '../foto/foto.component';
+import { PainelComponent } from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
@@ -8,15 +10,42 @@ import { Http } from '@angular/http';
 })
 
 export class ListagemComponent {
-    fotos: Object[] = [];
+    fotos: FotoComponent[] = [];
+    service: FotoService;
+    mensagem: string = '';
 
-    constructor(http: Http){
-        http
-        .get('v1/fotos')
-        .map(res=>  res.json())
-        .subscribe(fotos => {
-            this.fotos = fotos;
-           console.log(this.fotos);
-        }, erro => console.log(erro));
+    constructor(service: FotoService){
+        this.service = service;
+        service
+            .lista()
+            .subscribe(fotos => {
+                this.fotos = fotos;
+            }, erro => console.log(erro));
+    }
+
+    remove(foto, painel: PainelComponent){
+
+        
+            this.service
+            .remove(foto)
+            .subscribe(
+                () =>  {
+                    painel.fadeOut(() => {
+                        let novasFotos = this.fotos.slice(0);
+                        let indice = novasFotos.indexOf(foto);
+                        novasFotos.splice(indice, 1);
+                        this.fotos = novasFotos;
+                        this.mensagem = 'Foto removida com sucesso';
+                    });
+
+                }, 
+                
+                erro =>  {
+                        console.log(erro);
+                        this.mensagem = "Nao foi possivel remover a foto";
+                         }
+                );
+       
+       
     }
 }
