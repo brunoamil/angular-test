53866dd97f322d9dbbe3063c4c2e3c5dad7252b7
Bruno Lima <bruno-amil@hotmail.com>
2020-06-11 20:42:10 -0300

    implementacao do filtro e paginacao

--- insta-photo/src/app/photos/photo-list/photo-list.component.ts
+++ insta-photo/src/app/photos/photo-list/photo-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-  photos: any[] = [];
-    
-    constructor(private photoService: PhotoService, private activateRoute: ActivatedRoute){} 
+  photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  
+    constructor(private activateRoute: ActivatedRoute){} 
 
     ngOnInit(): void {
-      
-      const userName = this.activateRoute.snapshot.params.userName;
 
-      this.photoService
-          .listFromUser(userName)
-          .subscribe(photos => this.photos = photos);
+      this.photos = this.activateRoute.snapshot.data.photos;
+      this.debounce
+        .pipe(debounceTime(300)).subscribe(filter => this.filter = filter);
+    }
+
+    ngOnDestroy(): void {
+      this.debounce.unsubscribe();
     }
 
 }
