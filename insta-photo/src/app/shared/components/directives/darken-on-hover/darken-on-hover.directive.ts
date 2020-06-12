ddbeee7409d80d3fcfea22316f508344c0566497
Bruno Lima <bruno-amil@hotmail.com>
2020-06-11 23:07:12 -0300

    criacao de diretivas

---
+++ insta-photo/src/app/shared/components/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, HostListener, Renderer, Input } from "@angular/core";
+
+@Directive({
+    selector: '[apDarkOnHover]'
+})
+export class DarkenOnHoverDirective {
+
+    @Input() brightness = '70%';
+
+    constructor(private el: ElementRef, private render: Renderer) {
+
+    }
+
+    @HostListener('mouseover')
+    darkenOne() {
+        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+    @HostListener('mouseleave')
+    darkenOff() {
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    }
+}
