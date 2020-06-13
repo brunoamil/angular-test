159368e500b10d51c94d27a2f540a2820bb6c698
Bruno Lima <bruno-amil@hotmail.com>
2020-06-13 12:13:30 -0300

    melhorando o projeto com code spliting

--- insta-photo/src/app/home/home.module.ts
+++ insta-photo/src/app/home/home.module.ts
@@ -5,15 +5,23 @@ import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home.routing.module';
+import { SignUpService } from './signup/signup.service';
 
 @NgModule({
-    declarations: [ SignInComponent, SignUpComponent ],
+    declarations: [ SignInComponent, SignUpComponent, 
+        HomeComponent],
     imports: [ 
         CommonModule, 
         ReactiveFormsModule,
         VMessageModule,
         RouterModule,
-        FormsModule
+        FormsModule,
+        HomeRoutingModule
+    ],
+    providers: [
+        SignUpService
     ]
 })
 export class HomeModule { }
--- insta-photo/src/app/home/signup/signup.component.ts
+++ insta-photo/src/app/home/signup/signup.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 import { NewUser } from './new-user';
 import { SignUpService } from './signup.service';
 import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/plataform-detector/platform-detector.service';
 
 
 @Component({
-    templateUrl: './signup.component.html'
+    templateUrl: './signup.component.html',
+    providers: [ UserNotTakenValidatorService ]
 })
 export class SignUpComponent implements OnInit {
         //pegando valores do form
         signupForm: FormGroup;
+        @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
 
         constructor(private formBuilder: FormBuilder, 
                     private userNotTakenValidatorService: UserNotTakenValidatorService,
                     private signUpService: SignUpService,
-                    private router: Router){
+                    private router: Router,
+                    private platformDetectorService: PlatformDetectorService){
 
         }
     ngOnInit(): void {
@@ -51,7 +55,9 @@ export class SignUpComponent implements OnInit {
                     Validators.maxLength(14)
                 ]
             ]
-        })
+        });
+        this.platformDetectorService.isPlatformBrowser() && 
+        this.emailInput.nativeElement.focus();
     }
 
     signup(){
