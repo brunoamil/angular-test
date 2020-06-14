c90431f4580ea3436c1c23640b2e5ebc90e652a6
Bruno Lima <bruno-amil@hotmail.com>
2020-06-14 13:17:07 -0300

    renderizando as fotos

---
+++ insta-photo/src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+import { Observable } from 'rxjs';
+
+@Component({
+    templateUrl: './photo-deitals.component.html',
+    styleUrls: ['photo-details.css']
+})
+export class PhotoDetailsComponent implements OnInit {
+
+    photo$: Observable<Photo>;
+
+    constructor(private route: ActivatedRoute, private photoService: PhotoService){}
+    ngOnInit(): void {
+        this.photo$ = this.photoService.findById(this.route.snapshot.params.photoId);
+    }
+}
