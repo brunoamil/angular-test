7d655c6726e8d45e57681bb2e4e3e2d49475d777
Bruno Lima <bruno-amil@hotmail.com>
2020-06-14 12:38:34 -0300

    organizando as rotas

--- insta-photo/src/app/photos/photo-form/photo-form.module.ts
+++ insta-photo/src/app/photos/photo-form/photo-form.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule ]
+    imports: [ CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule, PhotoModule,
+    ImmediateClickModule ]
 })
 export class PhotoFormModule { }
