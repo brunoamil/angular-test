1bf497a94e0191a256c6bc965436ad68babf9abe
Bruno Lima <bruno-amil@hotmail.com>
2020-06-22 19:59:33 -0300

    testando de novo alguns componentes do angular

---
+++ pic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bruno';
+  description= 'Ekko';
+  url = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ekko_28.jpg";
+}
