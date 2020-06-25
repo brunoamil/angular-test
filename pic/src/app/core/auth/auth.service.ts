85a195bdf419f67746df43927bd5bf7cc56c62db
Bruno Lima <bruno-amil@hotmail.com>
2020-06-24 23:51:47 -0300

    criacao do authguard

--- pic/src/app/core/auth/auth.service.ts
+++ pic/src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API = 'http://localhost:3000'
 @Injectable({
@@ -9,14 +9,14 @@ const API = 'http://localhost:3000'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private TokenService: TokenService) { }
+  constructor(private http: HttpClient, private userService: UserService ) { }
 
   authenticate(userName: string, password: string) {
       return this.http
             .post(API + '/user/login', { userName, password}, { observe: 'response'})
             .pipe(tap(res => {
                const authToken = res.headers.get('x-access-token');
-               this.TokenService.setToken(authToken);
+               this.userService.setToken(authToken);
                //window.localStorage.setItem('authToken', authToken);
                console.log(`Usuário ${userName} autenticado com o token ${authToken}`);
             }))
