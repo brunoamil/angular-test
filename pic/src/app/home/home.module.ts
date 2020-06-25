8adaf947ca9735a0694fdec74ce3f79bf5351481
Bruno Lima <bruno-amil@hotmail.com>
2020-06-25 20:10:34 -0300

    validacado do form

--- pic/src/app/home/home.module.ts
+++ pic/src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 
 import { SigInComponent } from "./signin/signin.component";
-import { ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignUpComponent } from "./signup/signup.component";
 
 @NgModule({
-    declarations: [ SigInComponent ],
-    imports: [ CommonModule, ReactiveFormsModule, VMessageModule, RouterModule ]
+    declarations: [ SigInComponent, SignUpComponent ],
+    imports: [ CommonModule, ReactiveFormsModule, VMessageModule, RouterModule, FormsModule ]
 })
 export class HomeModule {}
