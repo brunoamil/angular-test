e031564e54e768b9ad2f7bf14410fd50194d5f7e
Bruno Lima <bruno-amil@hotmail.com>
2020-06-26 22:33:40 -0300

    criacao do component details

---
+++ pic/src/app/photos/photo-detail/photo-detail.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { PhotoDetailComponent } from "./photo-detail.componet";
+import { CommonModule } from "@angular/common";
+import { PhotoModule } from "../photo/photo.module";
+
+@NgModule({
+    declarations: [ PhotoDetailComponent],
+    exports: [ PhotoDetailComponent],
+    imports: [CommonModule, PhotoModule]
+})
+export class PhotoDetailModule {}
