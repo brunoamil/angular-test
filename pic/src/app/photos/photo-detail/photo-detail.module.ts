4017c8c9ceac17f078e4ce5c9d8a548b47a7472e
Bruno Lima <bruno-amil@hotmail.com>
2020-06-26 23:41:53 -0300

    criando o component comentarios

--- pic/src/app/photos/photo-detail/photo-detail.module.ts
+++ pic/src/app/photos/photo-detail/photo-detail.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { PhotoDetailComponent } from "./photo-detail.componet";
 import { CommonModule } from "@angular/common";
 import { PhotoModule } from "../photo/photo.module";
+import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 
 @NgModule({
-    declarations: [ PhotoDetailComponent],
-    exports: [ PhotoDetailComponent],
-    imports: [CommonModule, PhotoModule]
+    declarations: [ PhotoDetailComponent, PhotoCommentsComponent],
+    exports: [ PhotoDetailComponent, PhotoCommentsComponent],
+    imports: [CommonModule, PhotoModule, RouterModule, ReactiveFormsModule, VMessageModule]
 })
 export class PhotoDetailModule {}
