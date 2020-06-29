9847cafcc7d58467d0d6e37400d0134a426f8a2f
Bruno Lima <bruno-amil@hotmail.com>
2020-06-29 17:45:37 -0300

    criacao do menu

--- pic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ pic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -6,6 +6,7 @@ import { UserService } from "src/app/core/user/user.service";
 })
 export class ShowLoggedDirective implements OnInit {
 
+    currentDisplay: string;
 
     constructor(
     private element: ElementRef<any>,
@@ -13,6 +14,15 @@ export class ShowLoggedDirective implements OnInit {
     private userService: UserService){}
 
     ngOnInit(): void {
-       !this.userService.isLogged() && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        this.userService.getUser().subscribe(user => {
+            if(user) {
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', this.currentDisplay);
+            } else {
+                this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            }
+        })
+      
     }
 }
